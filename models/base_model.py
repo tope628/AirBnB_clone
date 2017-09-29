c87114cf26a56474502ed695be5bddd0a787dd86
Tope <tope628@gmail.com>
2017-09-29 22:27:55 +0000

    pep8 fixes

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 this module contains the class base_model
 '''
 import uuid
-import datetime
+import datetime as DT
 
 
 class BaseModel:
@@ -15,16 +15,16 @@ class BaseModel:
         '''method: __init__
         initializes instance of class BaseModel
         '''
-        if len(kwargs) > 0: # if kwargs exist, then load kwargs into __dict__
+        if len(kwargs) > 0:  # if kwargs exist, then load kwargs into __dict__
             self.__dict__.update(kwargs)
-            self.created_at = datetime.datetime.strptime(self.created_at,
-                                                '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.datetime.strptime(self.updated_at,
-                                                '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = DT.datetime.strptime(self.created_at,
+                                                   '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = DT.datetime.strptime(self.updated_at,
+                                                   '%Y-%m-%dT%H:%M:%S.%f')
             del self.__dict__["__class__"]
-        else: # if no kwargs, then this is inits a new object                
+        else:  # if no kwargs, then this is inits a new object
             self.id = str(uuid.uuid4())
-            time_stamp = datetime.datetime.now()
+            time_stamp = DT.datetime.now()
             self.created_at = time_stamp
             self.updated_at = time_stamp
 
@@ -57,7 +57,7 @@ class BaseModel:
         #print("debug: self.__dict__:{}".format(self.__dict__))
         for key, val in self.__dict__.items():
             new_dict[key] = val
-        new_dict["__class__"] = self.__class__.__name__    
+        new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.__dict__['created_at'].isoformat()
         new_dict["updated_at"] = self.__dict__['updated_at'].isoformat()
-        return new_dict 
+        return new_dict
