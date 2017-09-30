90f169962c48cf2634efed41badf324005729f64
Larry Madeo <lmadeo76@gmail.com>
2017-09-30 06:34:16 +0000

    added debug commits to test_save ...

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -3,6 +3,9 @@ from models import storage
 from models.base_model import BaseModel
 
 all_objs = storage.all()
+print("debug: {}".format(str(all_objs)))
+print(type(all_objs))
+
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
