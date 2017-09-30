24e74d7245f62d9894d0f7772eb5b1b6a6e448a5
Tope <tope628@gmail.com>
2017-09-30 22:04:42 +0000

    tope branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,7 +47,7 @@ class FileStorage:
         for key in self.__objects:
             new_dict[key]  = self.__objects[key].to_dict()
         json_str = json.dumps(new_dict)
-        with open(self.__file_path, mode='a', encoding='utf-8') as f:
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
             f.write(json_str)
 
     def reload(self):
@@ -56,8 +56,10 @@ class FileStorage:
         if JSON file exists, otherwise do nothing)
         '''
         try:
-            with open(self.__filepath, mode='r', encoding='utf-8') as f:
-                from_json = json.load(f)
-            return from_json
-        except:
-            pass
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                dict_from_json = json.load(f)
+            for k, v in dict_from_json.items():
+                new_obj = BaseModel(**v)
+                self.__objects[k] = new_obj
+        except FileNotFoundError:
+            return
