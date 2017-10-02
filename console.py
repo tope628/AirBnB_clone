1abc24faef52e6cf41a51cd1114bd6d7fcba3672
Larry Madeo <lmadeo76@gmail.com>
2017-10-02 05:00:37 +0000

    implemented: do-destroy

--- console.py
+++ console.py
@@ -73,12 +73,36 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-	def do_destroy(self, args):
-		'''method: do_delete
-		delete an object based on class name and id and update change to file
-		'''
-		list_args = args.split()
-		
+    def do_destroy(self, args):
+        '''method: do_delete
+        delete an object based on class name and id and update change to file
+        '''
+        list_args = args.split()
+        all_objs = models.storage.all()
+        if len(list_args) == 0:
+            print("** class name missing **")
+            return False
+
+        # build list of all classes of objects in storage
+        all_classes = []
+        for obj in all_objs:
+            a_class = obj.split(".")[0]
+            if a_class not in all_classes:
+                all_classes.append(a_class)
+
+        if list_args[0] in all_classes:
+            if len(list_args) > 1:
+                key = list_args[0] + "." + list_args[1]
+                if key in all_objs:
+                    # this is where we delete the ojbect from storage instance
+                    del models.storage.all()[key]
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id  missing **")
+        else:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
